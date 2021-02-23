8e43afeee1b5cff3c8227699d38d7b114c3ac16c
Documenter.jl <documenter@juliadocs.github.io>
2021-02-23 20:22:08 +0000

    build based on 8d44026

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GSDistributions","category":"page"},{"location":"#GSDistributions","page":"Home","title":"GSDistributions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Modules = [GSDistributions]","category":"page"},{"location":"#GSDistributions.GSDist","page":"Home","title":"GSDistributions.GSDist","text":"GSDist <: ContinuousUnivariateDistribution\n\nGeneralized S-Distribution for approximating univariate distributions.\n\n\n\n\n\n","category":"type"},{"location":"#Index","page":"Home","title":"Index","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
